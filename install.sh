9eacdd64e3e09bb4bd9fc69cd2544b325ca4ab94
root on purr <admins@moomers.org>
2023-01-10 20:44:55 -0600

    remove unused caddy stuff

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ source .env
 # for traefik acme certs
 mkdir -p ${STORAGE}/traefik/acme
 
-# for caddy
-mkdir -p ${STORAGE}/caddy/data
-mkdir -p ${STORAGE}/caddy/config
-
 # install the systemd unit file
 cp ${REPO}/compose.service /etc/systemd/system/
 systemctl daemon-reload
