72624be7667b5efb7fbab4247a42b39cbfe9f471
Igor Serebryany <igor47@moomers.org>
2024-03-13 10:58:14 -0700

    mailman: try listening on specific lmtp address

--- config/mailman.core/custom-entrypoint.sh
+++ config/mailman.core/custom-entrypoint.sh
@@ -6,8 +6,9 @@ set -e
 # https://github.com/maxking/docker-mailman/blob/main/core/docker-entrypoint.sh
 # leaving just the last few lines:
 
-# mailman seems to need it?
-apk add postfix
+# mailman seems to need postfix for postmap
+# we add ipython for the mailman shell
+apk add postfix ipython
 
 # Now chown the places where mailman wants to write stuff.
 chown -R mailman /mailman
